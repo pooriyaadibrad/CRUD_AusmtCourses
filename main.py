a27ec84fcb6af88890094d0ce8a3c0356ca1bc92
pooriya <adibradp@gmail.com>
2024-07-01 11:30:56 +0330

    hover for button be set

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ win.title('CRUD')
 win.iconbitmap('icons/Mainicon.ico')
 #win.attributes("-fullscreen", True)
 win.configure(background='#216ADE')
+#def
+def ChangeButtonStyleWithHover(e):
+    btnRegister.configure(fg='#822FC1',background='white')
+def ChangeButtonStyleWithHoverToSelf(e):
+    btnRegister.configure(fg='white',background='#822FC1')
 #txt
 txtName=Entry(win,width=15,bd=5,font=('arial',15,'bold'),fg='#216ADE',bg='white')
 txtName.place(x=100,y=100)
@@ -33,6 +38,7 @@ lblAge.place(x=20,y=280)
 
 #btn
 btnRegister=Button(win,text='Register',width=10,font=('arial',15,'bold'),fg='white',background='#822FC1')
-btnRegister.bind('<Enter>',)
+btnRegister.bind('<Enter>',ChangeButtonStyleWithHover)
+btnRegister.bind('<Leave>',ChangeButtonStyleWithHoverToSelf)
 btnRegister.place(x=125,y=350)
 win.mainloop()
