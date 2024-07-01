c33b2a8bec07bf42c56777ff7faf92c8a637278d
pooriya <adibradp@gmail.com>
2024-07-01 11:09:05 +0330

    main from be set

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+win=Tk()
+win.geometry("800x600")
+win.title('CRUD')
+win.iconbitmap('icons/Mainicon.ico')
+#win.attributes("-fullscreen", True)
+win.configure(background='#216ADE')
+win.mainloop()
